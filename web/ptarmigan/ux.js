2b791529dd975e3d654d846b908cdb8bf3c8ff53
chocolatejollis38@gmail.com <chocolatejollis38@gmail.com@3a9c69cb-7c6f-f57f-a18b-c68e2e6a17dc>
2013-03-24 22:30:40 +0000

    Initial contextmenu support
    git-svn-id: https://ptarmigan.googlecode.com/svn/trunk@349 3a9c69cb-7c6f-f57f-a18b-c68e2e6a17dc

--- web/ptarmigan/ux.js
+++ web/ptarmigan/ux.js
@@ -5,29 +5,29 @@
 
 function ux_menu_item (icon, caption, link)
 {
-	this.icon = icon;
-	this.caption = caption;
-	this.link = link;
+	ux_menu_item.icon = icon;
+	ux_menu_item.caption = caption;
+	ux_menu_item.link = link;
 	
 	return(this);
 }
 
 function ux_menu (id)
 {
-	this.id = id;
-	this.items = new Array();
+	ux_menu.id = id;
+	ux_menu.items = new Array();
 	
 	return(this);
 }
 
 ux_menu.prototype.add_item = function (menu_item) {
-	this.items.push(menu_item);
+	ux_menu.items.push(menu_item);
 }
 
 ux_menu.prototype.display = function (x, y) {
 	$('body').append('<div id="' + this.id + '" class="ux_menu"></div>');
 	
-	for(itm in this.items) {
+	for(itm in ux_menu.items) {
 		alert(itm.caption);
 	}
 }
